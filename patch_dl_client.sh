73f83397590191110ea56890f562e0e95efe265a
Christian F. Brinkmann <christian.brinkmann@rwth-aachen.de>
2026-06-23 19:20:02 +0200

    refactor(scripts): remove obsolete MPI_APPNUM patch_* scripts
    The MPMD split logic (color=MPI_UNDEFINED for the DL client) is
    now baked directly into dl_clients/dl_client.cpp and
    dl_clients/phydll_dl_client.py as part of the metadata
    handshake. The old sed-based one-shot patchers (patch_dl_client.sh,
    patch_py_client.sh, patch_py_client2.sh) are no longer needed
    and would silently regress the build if rerun.

--- patch_dl_client.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sed -i '/int\* appnum_ptr = nullptr;/,/MPI_Comm_split(MPI_COMM_WORLD, color, 0, &local_comm);/c\
-    // Participate in the MPMD split to avoid collective mismatches.\
-    // We no longer rely on MPI_APPNUM, because Slurm srun with OpenMPI 5 assigns appnum 0 to both components!\
-    // Since this binary is ALWAYS the DL client, we unconditionally assign it color MPI_UNDEFINED.\
-    const int color = MPI_UNDEFINED;\
-    MPI_Comm local_comm = MPI_COMM_NULL;\
-    MPI_Comm_split(MPI_COMM_WORLD, color, 0, &local_comm);' /rwthfs/rz/cluster/hpcwork/ro092286/smartsim/CPP-ML-Interface/dl_clients/dl_client.cpp
--- patch_py_client.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sed -i '/appnum = world_comm.Get_attr(MPI.APPNUM)/,/local_comm = world_comm.Split(color, 0)/c\
-    # Handle MPMD split\
-    # We no longer rely on MPI_APPNUM, because Slurm srun with OpenMPI 5 assigns appnum 0 to both components!\
-    # Since this script is ALWAYS the DL client, we unconditionally assign it color MPI_UNDEFINED.\
-    color = MPI.UNDEFINED\
-    local_comm = world_comm.Split(color, 0)' /rwthfs/rz/cluster/hpcwork/ro092286/smartsim/CPP-ML-Interface/dl_clients/phydll_dl_client.py
